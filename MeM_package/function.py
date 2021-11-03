42f169af21f48754086535498879b8ba7fab7686
Enricograndi <enrico.grandi@student.h-farm.com>
2021-11-03 19:06:12 +0100

    Create documentation and some issues

--- MeM_package/function.py
+++ MeM_package/function.py
@@ -1,3 +1,11 @@
+"""
+.. module:: function
+   :synopsis: All function of the project are defined here
+"""
+
+
+
+
 import os
 from binance.client import Client
 import datetime
@@ -6,10 +14,8 @@ from dateutil.relativedelta import relativedelta
 from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
-
 binance_api = ""   # Here API key from private binance account
 secrete_key = ""   # Here secrete key from private binance account
-
 # init
 api_key = os.environ.get(binance_api)
 # Set account, it allows to connect the code with the Web
@@ -17,8 +23,12 @@ api_secret = os.environ.get(secrete_key)  # Set account
 client = Client(api_key, api_secret)   # Login
 
 def create_timestamp(date):
-    'This function translate the gregorian'
-    'data format into Unix Timestamp usefull to call Binance API'
+    """This function translate the gregorian
+    data format into Unix Timestamp usefull to call Binance API
+
+    :return: The day
+    :rtype: Timestamp
+    """
     # Remove space and concatenate with second and milliseconds
     date_string = (date.replace(" ", "") + ", 00:00:00")
     # Transform the string in a DataTime object
@@ -29,8 +39,13 @@ def create_timestamp(date):
     return str(timestamp)
 
 def create_interval(given_date, end):
-    'This function creates a new dictionary containing'
-    'as key the gregorian value and as value the timestamp'
+    """This function creates a new dictionary containing
+    'as key the gregorian value and as value the timestamp
+
+    :return: The values
+    :rtype: Dictionary
+    """
+    
     # Create the dictionary object
     interval = {}
     # Save the start date
@@ -46,25 +61,29 @@ def create_interval(given_date, end):
         # Add one day to the Data Obj
         future_date = dtObj + relativedelta(days=x)
         # Transform DataObj to a string
-        future_date_str = future_date.strftime('%m/%d/%Y')
+        future_date_str = future_date.strftime(date_format)
         # Convert the string into Tinestamp and save it
         interval[future_date_str] = {create_timestamp(future_date_str)}
     # Return the Dicitionary
     return interval
 
 def retrieve_binance_data(interval):
-    'This function retrieves the data from the binance'
-    'servers for a given interval of time'
+    """This function retrieves the data from the binance 
+    'servers for a given interval of time
+
+    :return: The values
+    :rtype: List"""
+
     data = []
     dates = interval.keys()
     for date in dates:
         # print(date)
         timestamp = str(interval[date]).replace("{",
                                                 "").replace("}",
-                                                "").replace("'",
-                                                            "")
+                                                            "").replace("'",
+                                                                        "")
         new_data = client.get_historical_klines('BTCUSDT',
-                                                '1M', 
+                                                '1M',
                                                 timestamp, 
                                                 limit=1000)
         data = data+new_data
---
+++ docs/source/index.rst
@@ -0,0 +1,10 @@
+Documentation for the Code
+**************************
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+MeM_package function
+=====================
+.. automodule:: function
+   :members:
